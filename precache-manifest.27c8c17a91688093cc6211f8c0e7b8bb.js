f0f405cb8aeb450e65bce30899add4be9974e122
nidheeshmd <md.nidheesh3@gmail.com>
2020-07-09 09:32:43 +0200

    Updates

---
+++ precache-manifest.27c8c17a91688093cc6211f8c0e7b8bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50cd89243d3252f1cde32de4029b9ea",
+    "url": "/weather-finder-app/index.html"
+  },
+  {
+    "revision": "ba8d0d78f1b84476fa53",
+    "url": "/weather-finder-app/static/css/main.c48f156f.chunk.css"
+  },
+  {
+    "revision": "4189eabcd778fc693f12",
+    "url": "/weather-finder-app/static/js/2.09472466.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/weather-finder-app/static/js/2.09472466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8d0d78f1b84476fa53",
+    "url": "/weather-finder-app/static/js/main.5adc55f4.chunk.js"
+  },
+  {
+    "revision": "da5bb7587a49994df70c",
+    "url": "/weather-finder-app/static/js/runtime-main.2232917c.js"
+  }
+]);
